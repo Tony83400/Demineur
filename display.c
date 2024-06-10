280d88ee1853321ab008f88ec81afa56dee929a9
Tony83400 <100167075+Tony83400@users.noreply.github.com>
2024-06-10 10:46:25 +0200

    Update display.c

--- display.c
+++ display.c
@@ -6,14 +6,6 @@
 #include "GFXlib/ESLib.h" // Pour utiliser valeurAleatoire()
 #include "Fonction.h"
 
-#include <stdlib.h> // Pour pouvoir utiliser exit()
-#include <stdio.h> // Pour pouvoir utiliser printf()
-#include <math.h> // Pour pouvoir utiliser sin() et cos()
-#include "GFXlib/GfxLib.h" // Seul cet include est necessaire pour faire du graphique
-#include "GFXlib/BmpLib.h" // Cet include permet de manipuler des fichiers BMP
-#include "GFXlib/ESLib.h" // Pour utiliser valeurAleatoire()
-#include "Fonction.h"
-
 /* La fonction de gestion des evenements, appelee automatiquement par le systeme
 des qu'une evenement survient */
 void gestionEvenement(EvenementGfx evenement)
