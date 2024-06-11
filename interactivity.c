a3de4b19eb41cf938290b85b5076e8e735810c2a
Tony83400 <100167075+Tony83400@users.noreply.github.com>
2024-06-11 14:45:49 +0200

    Update interactivity.c

---
+++ interactivity.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>        // Pour pouvoir utiliser exit()
+#include <stdio.h>         // Pour pouvoir utiliser printf()
+#include <math.h>          // Pour pouvoir utiliser sin() et cos()
+#include "GFXlib/GfxLib.h" // Seul cet include est necessaire pour faire du graphique
+#include "GFXlib/BmpLib.h" // Cet include permet de manipuler des fichiers BMP
+#include "GFXlib/ESLib.h"  // Pour utiliser valeurAleatoire()
+#include "Fonction.h"
+
+bool targetMouse(coo *coord, int nbColonne, int nbLigne)
+{
+    if (abscisseSouris() > largeurFenetre() / 2 - (COTE_IMAGE * nbColonne) / 2 && abscisseSouris() < largeurFenetre() / 2 + (COTE_IMAGE * nbColonne) / 2 && ordonneeSouris() > hauteurFenetre() / 2 - (COTE_IMAGE * nbLigne) / 2 && ordonneeSouris() < hauteurFenetre() / 2 + (COTE_IMAGE * nbLigne) / 2)
+    {
+        coord->y = ((abscisseSouris() - (largeurFenetre() / 2 - (COTE_IMAGE * nbColonne) / 2)) / COTE_IMAGE) + 1;
+        coord->x = nbLigne - ((ordonneeSouris() - (hauteurFenetre() / 2 - (COTE_IMAGE * nbLigne) / 2)) / COTE_IMAGE);
+        printf("x : %d\ny : %d \n", coord->x, coord->y);
+        return true;
+    }
+    return false;
+}
+
