aae1bb096d214f61c338c62f31d5a5016d3cd779
Tony83400 <100167075+Tony83400@users.noreply.github.com>
2024-06-14 13:45:36 +0200

    Update Fonction.h

--- Fonction.h
+++ Fonction.h
@@ -8,38 +8,37 @@
 #define GAP_FACILE 300
 #define COTE_IMAGE 32
 
-#define LONGUEUR 17
-#define LARGEUR 12
+#define LONGUEUR 27
+#define LARGEUR 22
 
 typedef struct cell
 {
-	int number;
-	int revealed;
-	int flag;
-	int bomb;
+    int number;
+    int revealed;
+    int flag;
+    int bomb;
     DonneesImageRGB *image;
     DonneesImageRGB *imageDepart;
-}cell;
-
-
+} cell;
 
 typedef struct coo
 {
     int x;
     int y;
-}coo;
+} coo;
 
 int verifVoisin(cell tab[LONGUEUR][LARGEUR], int x, int y);
-void initTab(cell tab[LONGUEUR][LARGEUR], int  x, int  y, int difficult);
-void afficheTab(cell tab[LONGUEUR][LARGEUR], int  x, int  y);
-void initNumber(cell tab[LONGUEUR][LARGEUR]);
+void initTab(cell tab[LONGUEUR][LARGEUR], int x, int y, int difficulty, int nbBomb);
+void afficheTab(cell tab[LONGUEUR][LARGEUR], int difficulty);
+void initNumber(cell tab[LONGUEUR][LARGEUR], int difficulty);
 int nbBombe(cell tab[LONGUEUR][LARGEUR]);
 int aGagne(cell tab[LONGUEUR][LARGEUR]);
 int aPerdu(cell tab[LONGUEUR][LARGEUR], int x, int y);
-void flager(cell tab[LONGUEUR][LARGEUR], int x, int y);
+void flager(cell tab[LONGUEUR][LARGEUR], int x, int y, int difficulty, int *nbFlag);
+void revealer(cell tab[LONGUEUR][LARGEUR], int x, int y, int difficulty);
+void copieList(coo tab1[LARGEUR * LONGUEUR], coo tab2[LARGEUR * LONGUEUR], int difficulty);
 
-
-bool targetMouse(coo * coord, int nbColonne, int nbLigne);
+bool targetMouse(coo *coord, int difficulty);
 void cercle(float centreX, float centreY, float rayon);
 void gestionEvenement(EvenementGfx evenement);
 void initImage(cell plateau[LONGUEUR][LARGEUR], int difficulty);
