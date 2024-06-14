4fd8dc4c6ff3289da6ce062e2d719163d0f25e37
Tony83400 <100167075+Tony83400@users.noreply.github.com>
2024-06-14 13:46:22 +0200

    Update interactivity.c

--- interactivity.c
+++ interactivity.c
@@ -6,15 +6,16 @@
 #include "GFXlib/ESLib.h"  // Pour utiliser valeurAleatoire()
 #include "Fonction.h"
 
-bool targetMouse(coo *coord, int nbColonne, int nbLigne)
+bool targetMouse(coo *coord, int difficulty)
 {
+    int nbColonne = 10 + difficulty * 5, nbLigne = 15 + difficulty * 5;
+
     if (abscisseSouris() > largeurFenetre() / 2 - (COTE_IMAGE * nbColonne) / 2 && abscisseSouris() < largeurFenetre() / 2 + (COTE_IMAGE * nbColonne) / 2 && ordonneeSouris() > hauteurFenetre() / 2 - (COTE_IMAGE * nbLigne) / 2 && ordonneeSouris() < hauteurFenetre() / 2 + (COTE_IMAGE * nbLigne) / 2)
     {
         coord->y = ((abscisseSouris() - (largeurFenetre() / 2 - (COTE_IMAGE * nbColonne) / 2)) / COTE_IMAGE) + 1;
         coord->x = nbLigne - ((ordonneeSouris() - (hauteurFenetre() / 2 - (COTE_IMAGE * nbLigne) / 2)) / COTE_IMAGE);
-        printf("x : %d\ny : %d \n", coord->x, coord->y);
+        // printf("target : x : %d y : %d \n", coord->x, coord->y);
         return true;
     }
     return false;
 }
-
