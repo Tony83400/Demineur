c5b6eceb7d1c13e9084493497c646c8fa9dc445c
Tony83400 <100167075+Tony83400@users.noreply.github.com>
2024-06-16 09:45:53 +0200

    Update Fonction.h

--- Fonction.h
+++ Fonction.h
@@ -28,20 +28,23 @@ typedef struct coo
 } coo;
 
 int verifVoisin(cell tab[LONGUEUR][LARGEUR], int x, int y);
-void initTab(cell tab[LONGUEUR][LARGEUR], int x, int y, int difficulty, int nbBomb);
+void initTab(cell tab[LONGUEUR][LARGEUR], int difficulty);
 void afficheTab(cell tab[LONGUEUR][LARGEUR], int difficulty);
 void initNumber(cell tab[LONGUEUR][LARGEUR], int difficulty);
 int nbBombe(cell tab[LONGUEUR][LARGEUR]);
 int aGagne(cell tab[LONGUEUR][LARGEUR]);
 int aPerdu(cell tab[LONGUEUR][LARGEUR], int x, int y);
-void flager(cell tab[LONGUEUR][LARGEUR], int x, int y, int difficulty, int *nbFlag);
+void flager(cell tab[LONGUEUR][LARGEUR], int x, int y, int *nbFlag);
 void revealer(cell tab[LONGUEUR][LARGEUR], int x, int y, int difficulty);
 void copieList(coo tab1[LARGEUR * LONGUEUR], coo tab2[LARGEUR * LONGUEUR], int difficulty);
+void revealBomb(cell tab[LONGUEUR][LARGEUR], int difficulty);
+void explosion(cell tab[LONGUEUR][LARGEUR], int difficulty, int x, int y);
+void initBomb(cell tab[LONGUEUR][LARGEUR], int x, int y, int difficulty, int nbBomb);
 
 bool targetMouse(coo *coord, int difficulty);
-void cercle(float centreX, float centreY, float rayon);
 void gestionEvenement(EvenementGfx evenement);
 void initImage(cell plateau[LONGUEUR][LARGEUR], int difficulty);
 void quadrillage(cell plateau[LONGUEUR][LARGEUR], int diffculty);
+void infoCase(cell tab[LONGUEUR][LARGEUR], int x, int y);
 
 #endif
