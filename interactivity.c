3b54de1e565715909feef4d9e1d9b115ebbdc2a3
Tony83400 <100167075+Tony83400@users.noreply.github.com>
2024-06-17 11:12:45 +0200

    Update interactivity.c

--- interactivity.c
+++ interactivity.c
@@ -6,14 +6,15 @@
 #include "GFXlib/ESLib.h"  // Pour utiliser valeurAleatoire()
 #include "Fonction.h"
 
-bool targetMouse(coo *coord, int difficulty)
+
+bool targetMouse(coo *coord, int difficulty,int tailleImage)
 {
     int nbColonne = 10 + difficulty * 5, nbLigne = 15 + difficulty * 5;
 
-    if (abscisseSouris() > largeurFenetre() / 2 - (COTE_IMAGE * nbColonne) / 2 && abscisseSouris() < largeurFenetre() / 2 + (COTE_IMAGE * nbColonne) / 2 && ordonneeSouris() > hauteurFenetre() / 2 - (COTE_IMAGE * nbLigne) / 2 && ordonneeSouris() < hauteurFenetre() / 2 + (COTE_IMAGE * nbLigne) / 2)
+    if (abscisseSouris() > largeurFenetre() / 2 - (tailleImage * nbColonne) / 2 && abscisseSouris() < largeurFenetre() / 2 + (tailleImage * nbColonne) / 2 && ordonneeSouris() > hauteurFenetre() / 2 - (tailleImage * nbLigne) / 2 && ordonneeSouris() < hauteurFenetre() / 2 + (tailleImage * nbLigne) / 2)
     {
-        coord->y = ((abscisseSouris() - (largeurFenetre() / 2 - (COTE_IMAGE * nbColonne) / 2)) / COTE_IMAGE) + 1;
-        coord->x = ((ordonneeSouris() - (hauteurFenetre() / 2 - (COTE_IMAGE * nbLigne) / 2)) / COTE_IMAGE)+1;
+        coord->y = ((abscisseSouris() - (largeurFenetre() / 2 - (tailleImage * nbColonne) / 2)) / tailleImage) + 1;
+        coord->x = ((ordonneeSouris() - (hauteurFenetre() / 2 - (tailleImage * nbLigne) / 2)) / tailleImage)+1;
         // printf("target : x : %d y : %d \n", coord->x, coord->y);
         return true;
     }
