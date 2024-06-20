809a5ed42150880024a7911a857b29f99588d892
Tony83400 <100167075+Tony83400@users.noreply.github.com>
2024-06-20 15:56:45 +0200

    j'ai tout whippin à l'oublier

--- interactivity.c
+++ interactivity.c
@@ -6,16 +6,23 @@
 #include "GFXlib/ESLib.h"  // Pour utiliser valeurAleatoire()
 #include "Fonction.h"
 
-
-bool targetMouse(coo *coord, int difficulty,int tailleImage)
+bool targetMouse(coo *coord, int difficulty, int tailleImage)
 {
     int nbColonne = 10 + difficulty * 5, nbLigne = 15 + difficulty * 5;
 
     if (abscisseSouris() > largeurFenetre() / 2 - (tailleImage * nbColonne) / 2 && abscisseSouris() < largeurFenetre() / 2 + (tailleImage * nbColonne) / 2 && ordonneeSouris() > hauteurFenetre() / 2 - (tailleImage * nbLigne) / 2 && ordonneeSouris() < hauteurFenetre() / 2 + (tailleImage * nbLigne) / 2)
     {
         coord->y = ((abscisseSouris() - (largeurFenetre() / 2 - (tailleImage * nbColonne) / 2)) / tailleImage) + 1;
-        coord->x = ((ordonneeSouris() - (hauteurFenetre() / 2 - (tailleImage * nbLigne) / 2)) / tailleImage)+1;
+        coord->x = ((ordonneeSouris() - (hauteurFenetre() / 2 - (tailleImage * nbLigne) / 2)) / tailleImage) + 1;
         // printf("target : x : %d y : %d \n", coord->x, coord->y);
+        return true;
+    }
+    return false;
+}
+bool cliqueButton(int x1Button, int y1Button, int x2Button, int y2Button)
+{
+    if (abscisseSouris() > x1Button && abscisseSouris() < x2Button && ordonneeSouris() > y1Button && ordonneeSouris() < y2Button)
+    {
         return true;
     }
     return false;
